e0bf249d69ab10515280fcab3a2ddf7ffcd47694
reniris <nyoronu@gmail.com>
2018-11-12 01:33:33 +0900

    母港情報の表示を追加

---
+++ KanburaLike/KanburaLike/ViewModels/HomeportViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Livet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanburaLike.ViewModels
+{
+	public class HomeportViewModel : Livet.ViewModel
+	{
+		public AdmiralViewModel Admiral { get; private set; } = new AdmiralViewModel();
+
+		public SlotItemsViewModel SlotItems { get; private set; } = new SlotItemsViewModel();
+
+
+		#region ShipsCount変更通知プロパティ
+		private int _ShipsCount;
+
+		public int ShipsCount
+		{
+			get
+			{ return _ShipsCount; }
+			set
+			{ 
+				if (_ShipsCount == value)
+					return;
+				_ShipsCount = value;
+				RaisePropertyChanged(nameof(ShipsCount));
+			}
+		}
+		#endregion
+
+	}
+}
---
+++ KanburaLike/KanburaLike/ViewModels/SlotItemsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Grabacr07.KanColleWrapper;
+using Livet.EventListeners;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanburaLike.ViewModels
+{
+	public class SlotItemsViewModel : Livet.ViewModel
+	{
+		#region Count 変更通知プロパティ
+
+		private int _Count;
+
+		public int Count
+		{
+			get { return this._Count; }
+			set
+			{
+				if (this._Count != value)
+				{
+					this._Count = value;
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+
+		#endregion
+
+		public SlotItemsViewModel()
+		{
+			this.CompositeDisposable.Add(new PropertyChangedEventListener(KanColleClient.Current.Homeport.Itemyard)
+			{
+				{ nameof(Itemyard.SlotItemsCount), (sender, args) => this.Update() }
+			});
+			this.Update();
+		}
+
+		private void Update()
+		{
+			this.Count = KanColleClient.Current.Homeport.Itemyard.SlotItemsCount;
+		}
+	}
+}
